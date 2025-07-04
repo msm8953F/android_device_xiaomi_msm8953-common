6de2ca38ca9cae7d2b2c26385fb9132f2af85c32
frnwot <frnbuid2005@gmail.com>
2025-07-04 14:31:46 +0000

    msm8935-common: Remove Lineage HALs and back FCM 3
    Signed-off-by: frnwot <frnbuid2005@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,10 +71,6 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
